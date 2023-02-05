7c481f2414f89c3b92890a17084cd53428cf2d58
xuo <61861557+xuo-lynn@users.noreply.github.com>
2023-02-04 22:53:23 -0500

    Initial Commit

---
+++ src/main/java/com/company/SummativeProject/Summative1LynnHenryKarimSetMichaelSuniulApplication.java
@@ -0,0 +1,13 @@
+package com.company.SummativeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1LynnHenryKarimSetMichaelSuniulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1LynnHenryKarimSetMichaelSuniulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/SummativeProject/Summative1LynnHenryKarimSetMichaelSuniulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.SummativeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1LynnHenryKarimSetMichaelSuniulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
