6ef0bf0f3690fa66dde60c3ca98afcd46a92aa2c
xuo <61861557+xuo-lynn@users.noreply.github.com>
2023-02-08 04:26:11 -0500

    finished commit

--- src/main/java/com/company/SummativeProject/controller/wordController.java
+++ src/main/java/com/company/SummativeProject/controller/wordController.java
@@ -1,4 +1,5 @@
 package com.company.SummativeProject.controller;
+import com.company.SummativeProject.models.Words;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,28 +7,30 @@ import java.util.HashMap;
 
 @RestController
 public class wordController {
-    public HashMap<String, String> wordsDictionary = new HashMap<>();
+    public HashMap<Words, String> wordsDictionary = new HashMap<>();
 
 
     public void populateDictionary() {
-        wordsDictionary.put("Hello", "A commonly used greeting");
-        wordsDictionary.put("World", "Referring to the planet Earth");
-        wordsDictionary.put("Java", "A programming language or a type of coffee");
-        wordsDictionary.put("Spring", "A lukewarm floral season of the year or a framework in Java");
-        wordsDictionary.put("Boot", "A piece of footwear or a framework in Java");
-        wordsDictionary.put("Python", "A programming language or a large snake");
-        wordsDictionary.put("C#", "A programming language or a musical note");
-        wordsDictionary.put("Apple", "A company that makes computers or a fruit");
-        wordsDictionary.put("Amazon", "A company that sells books or a river");
-        wordsDictionary.put("Google", "A company that makes search engines or a number");
+        wordsDictionary.put(new Words("Hello"), "A greeting or a way to start a phone call");
+        wordsDictionary.put(new Words("World"), "Referring to the planet Earth");
+        wordsDictionary.put(new Words("Java"), "A programming language or a type of coffee");
+        wordsDictionary.put(new Words("Spring"), "A lukewarm floral season of the year or a framework in Java");
+        wordsDictionary.put(new Words("Boot"), "A piece of footwear or a framework in Java");
+        wordsDictionary.put(new Words("Python"), "A programming language or a large snake");
+        wordsDictionary.put(new Words("C#"), "A programming language or a musical note");
+        wordsDictionary.put(new Words("Apple"), "A company that makes computers or a fruit");
+        wordsDictionary.put(new Words("Amazon"), "A company that sells books or a river");
+        wordsDictionary.put(new Words("Google"), "A company that makes search engines or a number");
     }
 
-    @GetMapping(value = "/word") //returns a random word and its definition (refresh the page to get a new word)
-    public String getWord() {
+    //prints a random word and it's definition
+    @GetMapping(value = "/word")
+    public String getRandomWord() {
         populateDictionary();
         int random = (int) (Math.random() * wordsDictionary.size());
-        String word = (String) wordsDictionary.keySet().toArray()[random];
-        String definition = wordsDictionary.get(word);
-        return "Your word of the day is: " + word + ": " + definition;
+        String randomDefinition = wordsDictionary.values().toArray()[random].toString();
+        String randomWord = wordsDictionary.keySet().toArray()[random].toString();
+        return "The Word of the Day is: '" + randomWord + ": " + randomDefinition+ "'";
+
     }
 }
