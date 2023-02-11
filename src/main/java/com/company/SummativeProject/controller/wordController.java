3366fc7bb39ff3ddf7681ee6b966a8f420e1193a
xuo <61861557+xuo-lynn@users.noreply.github.com>
2023-02-10 22:39:17 -0500

    word controller fullrework

--- src/main/java/com/company/SummativeProject/controller/wordController.java
+++ src/main/java/com/company/SummativeProject/controller/wordController.java
@@ -1,45 +1,50 @@
 package com.company.SummativeProject.controller;
+import com.company.SummativeProject.models.Quote;
 import com.company.SummativeProject.models.Words;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 @RestController
 public class wordController {
-    public LinkedHashMap<Words, String> wordsDictionary = new LinkedHashMap<>();
 
+    List<Words> words = new ArrayList<Words>();
 
-    public void populateDictionary() {
-        wordsDictionary.put(new Words("Hello"), "A greeting or a way to start a phone call");
-        wordsDictionary.put(new Words("World"), "Referring to the planet Earth");
-        wordsDictionary.put(new Words("Java"), "A programming language or a type of coffee");
-        wordsDictionary.put(new Words("Spring"), "A lukewarm floral season of the year or a framework in Java");
-        wordsDictionary.put(new Words("Boot"), "A piece of footwear or a framework in Java");
-        wordsDictionary.put(new Words("Python"), "A programming language or a large snake");
-        wordsDictionary.put(new Words("C#"), "A programming language or a musical note");
-        wordsDictionary.put(new Words("Apple"), "A company that makes computers or a fruit");
-        wordsDictionary.put(new Words("Amazon"), "A company that sells books or a river");
-        wordsDictionary.put(new Words("Google"), "A company that makes search engines or a number");
+    public wordController() {
 
-        //set id for each word
-        int id = 1;
-        for (Words word : wordsDictionary.keySet()) {
-            word.setId(id);
-            id++;
-        }
+        Words Hello = new Words(1, "Hello", "A greeting or a way to start a phone call");
+        Words Goodbye = new Words(2, "World", "Referring to the planet Earth");
+        Words ThankYou = new Words(3, "Java", "A programming language or a coffee");
+        Words Please = new Words(4, "Spring", "A season or a framework");
+        Words Sorry = new Words(5, "Boot", "A piece of footwear or a way to start a program");
+        Words ExcuseMe = new Words(6, "Python", "A programming language or a large snake");
+        Words Yes = new Words(7, "C#", "A programming language or a musical note");
+        Words No = new Words(8, "Apple", "A company that makes phones or a fruit ");
+        Words I = new Words(9, "Amazon", "A company that sells everything or a river");
+        Words You = new Words(10, "Google", "A company that knows everything or a verb");
+
+        words.add(Hello);
+        words.add(Goodbye);
+        words.add(ThankYou);
+        words.add(Please);
+        words.add(Sorry);
+        words.add(ExcuseMe);
+        words.add(Yes);
+        words.add(No);
+        words.add(I);
+        words.add(You);
     }
-
-    //prints a random word and it's definition
     @GetMapping(value = "/word")
-    public String getRandomWord() {
-        populateDictionary();
-        int random = (int) (Math.random() * wordsDictionary.size());
-        int randomId = wordsDictionary.keySet().stream().mapToInt(Words::getId).toArray()[random];
-        String randomWord = wordsDictionary.keySet().toArray()[random].toString();
-        String randomDefinition = wordsDictionary.get(wordsDictionary.keySet().toArray()[random]);
-        return "The Word of the Day is: " + "[" + randomId + "] " + randomWord + ": " + randomDefinition;
-
-    }
+    @ResponseStatus(value = HttpStatus.OK)
+    public Words getRandomWord() {
+            Random r = new Random();
+            int sol = r.nextInt(10) + 1;
+            for (Words word : this.words) {
+                if (word.getId() == sol) return word;
+            }
+            return null;
+        }
 }
