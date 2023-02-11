db76ba713e0077468cb64c295eab23586e3df096
John Doe <mlh184@pitt.edu>
2023-02-10 21:04:47 -0500

    nice

--- src/test/java/com/company/SummativeProject/quoteControllerTest.java
+++ src/test/java/com/company/SummativeProject/quoteControllerTest.java
@@ -2,14 +2,18 @@ package com.company.SummativeProject;
 
 import com.company.SummativeProject.controller.quoteController;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@RunWith(SpringRunner.class)
+@WebMvcTest(quoteController.class)
 public class quoteControllerTest {
     // Wiring in the Mockmvc
     @Autowired
@@ -18,11 +22,16 @@ public class quoteControllerTest {
     // ObjectMapper used to convert Java into JSON and vice versa
     private ObjectMapper mapper = new ObjectMapper();
 
+    @Before
+    public void setUp() {
+        // To initialize the `wordsDictionary` before each test
+        quoteController controller = new quoteController();
+    }
     // Testing POST
     @Test
     public void shouldReturnQuote() throws Exception {
         quoteController quoteObject = new quoteController();
         String output = mapper.writeValueAsString(quoteObject);
-        mockMvc.perform(get("/quote")).andDo(print()).andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/quote")).andDo(print()).andExpect(status().isOk());
     }
 }
